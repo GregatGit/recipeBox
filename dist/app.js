93f9fd7a94d1dcefde20061ef94c08d9e0088d03
Greg Duncan <gregdd@outlook.com>
2017-06-24 08:13:47 +1000

    testing

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MainBoard from './components/mainboard'
 
 class App extends React.Component {
   constructor (props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <img alt="freeCodeCamp logo" src="https://www.freecodecamp.com/design-style-guide/img/freeCodeCamp-alternative.png" />
         <p>a freeCodeCamp challange</p>
         <p>by Greg Duncan</p>
+        <MainBoard />
       </div>
     )
   }
---
+++ dist/components/mainboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class MainBoard extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <div>Main - Board</div>
+    )
+  }
+}
+
+export default MainBoard
